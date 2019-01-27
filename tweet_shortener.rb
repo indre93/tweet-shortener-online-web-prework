3f7ba1eae9e06b04331159e00092b9161decc426
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:06:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,10 +4,13 @@ dictionary = {"hello" => "hi", ["to", "two", "to"] => "2", ["for", "four"] => "4
 
 
 def word_substituter(tweet, dictionary)
-
-  tweet.each do |word|
-    puts word
-  end
+  tweet_array = tweet.split(" ")
+  tweet_array.map do |word|
+    dictionary.each do |key, value|
+      word = value if word.downcase == key
+    end
+    word
+  end.join(" ")
 end
 
 def bulk_tweet_shortener
