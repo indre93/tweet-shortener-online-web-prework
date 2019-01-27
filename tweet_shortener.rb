347714fd74bc602af22431bbd1746b41036f4d11
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:11:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,7 +12,7 @@ dictionary = {
    "and" => "&"
  }
 
-def word_substituter(tweet)
+def word_substituter(tweet, dictionary)
   array = tweet.split(" ")
   array.map do |word|
     dictionary.each do |key, value|
