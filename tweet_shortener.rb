6360d35d2acdce08027fe614ddde6d4bf5f451f3
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:09:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,16 @@
 # Write your code here.
-dictionary = {"hello" => "hi", ["to", "two", "to"] => "2", ["for", "four"] => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
-
-
+dictionary = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@", 
+   "and" => "&"
+ }
 
 def word_substituter(tweet, dictionary)
   tweet_array = tweet.split(" ")
