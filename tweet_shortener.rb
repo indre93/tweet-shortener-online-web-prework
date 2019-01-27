dc6267ac2e51b5e86b2e152f42367ecf8b8b7e4f
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:39:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.chars.length > 140
+  if tweet.length.length > 140
     word_substituter(tweet)
   else
     tweet
