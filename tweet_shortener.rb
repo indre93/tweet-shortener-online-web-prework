57fb17c0ca515839d61664d5ddba159774d6e2b0
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:36:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,18 +17,6 @@ def dictionary
 
 
 def word_substituter(tweet)
-  dictionary = {
-     "hello" => "hi",
-     "to" => "2",
-     "two" => "2",
-     "too" => "2",
-     "for" => "4",
-     "four" => "4",
-     "be" => "b",
-     "you" => "u",
-     "at" => "@",
-     "and" => "&"
-   }
   array = tweet.split(" ")
   array.map do |word|
     dictionary.each do |key, value|
