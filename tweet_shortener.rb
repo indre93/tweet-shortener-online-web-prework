2a407a7bf01e5f0b1a16776a243e195643f9b807
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:31:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,9 +31,9 @@ end
 def selective_tweet_shortener(tweets)
   tweets.split(" ").map do |tweet|
     if tweet.length > 140
-      puts bulk_tweet_shortener(tweet)
+      bulk_tweet_shortener(tweet)
     else tweet.length <= 140
-      puts tweet
+      tweet
     end
   end
 end
